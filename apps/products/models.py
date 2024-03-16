765f1f89addbb8e78f62f7adc6e27249306d5728
Yogesh Chaudhari <yochaudhari@gmail.com>
2024-03-16 18:36:49 +0530

    API for Get Products and added generics methods

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    product_title = models.CharField(max_length=100)
+    product_desc = models.TextField(max_length=100)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_category = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_title
--- ecomapi/urls.py
+++ ecomapi/urls.py
@@ -32,7 +32,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # path("", include("apps.products.v1.urls")),
+    path("api/v1/", include("apps.products.urls")),
     re_path(r"^api-docs/$", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
 ]
